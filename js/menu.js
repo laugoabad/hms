511d5dd23249c90f8b2bbf3949ce82353dec3348
laugoabad <36644178+laugoabad@users.noreply.github.com>
2019-11-09 04:18:59 -0300

    g

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
     $('#hamburguesa').click(function() {
        if ($('#hamburguesa').attr('class') == 'checkHamb') {
-           $('.hamb').css({'opacity' : '0'});
-           $('.cross').css({'opacity' : '1'});
-           $('.menu-link').css({ 'left': '0%' });
-           $('#hamburguesa').removeClass('checkHamb');
+           abrir()
        }
        else {
-           $('#hamburguesa').addClass('checkHamb');
-           $('.hamb').css({ 'opacity': '1' });
-           $('.cross').css({ 'opacity': '0' });
-           $('.menu-link').css({ 'left': '-100%' });
+           cerrar()
        }
     })
+
+    $('.menu-link').click(function() {
+        cerrar()
+    })
+
+    var cerrar = function() {
+        $('#hamburguesa').addClass('checkHamb');
+        $('.hamb').css({ 'opacity': '1' });
+        $('.cross').css({ 'opacity': '0' });
+        $('.menu-link').css({ 'left': '-100%' });
+    }
+
+    var abrir = function() {
+        $('.hamb').css({ 'opacity': '0' });
+        $('.cross').css({ 'opacity': '1' });
+        $('.menu-link').css({ 'left': '0' });
+        $('#hamburguesa').removeClass('checkHamb');
+    }
 })
